69258899ff2a5e71aec274650ae69b321080805a
ds-aachuki <aachuki.agarwal@dotsquares.com>
2025-07-22 15:36:17 +0530

    add permission config file

---
+++ config/permissions.php
@@ -0,0 +1,98 @@
+<?php
+
+return [
+    'admin' => [
+        'permissions' => [
+            'Dashboard' => [
+                ['name' => 'Dashboard', 'slug' => 'dashboard'],
+            ],
+
+            'Admin Manager' => [
+                ['name' => 'Admin Manager List', 'slug' => 'admin_manager_list'],
+                ['name' => 'Admin Manager Create', 'slug' => 'admin_manager_create'],
+                ['name' => 'Admin Manager View', 'slug' => 'admin_manager_view'],
+                ['name' => 'Admin Manager Edit', 'slug' => 'admin_manager_edit'],
+                ['name' => 'Admin Manager Delete', 'slug' => 'admin_manager_delete'],
+            ],
+
+            'Roles Manager' => [
+                ['name' => 'Roles Manager List', 'slug' => 'roles_manager_list'],
+                ['name' => 'Roles Manager Create', 'slug' => 'roles_manager_create'],
+                ['name' => 'Roles Manager Edit', 'slug' => 'roles_manager_edit'],
+                ['name' => 'Roles Manager View', 'slug' => 'roles_manager_view'],
+                ['name' => 'Roles Manager Delete', 'slug' => 'roles_manager_delete'],
+                ['name' => 'Assign Permission', 'slug' => 'assign_permission'],
+                ['name' => 'Assign Roles', 'slug' => 'assign_roles'],
+            ],
+
+            'Permission Manager' => [
+                ['name' => 'Permission Manager List', 'slug' => 'permission_manager_list'],
+                ['name' => 'Permission Manager Create', 'slug' => 'permission_manager_create'],
+                ['name' => 'Permission Manager Edit', 'slug' => 'permission_manager_edit'],
+                ['name' => 'Permission Manager View', 'slug' => 'permission_manager_view'],
+                ['name' => 'Permission Manager Delete', 'slug' => 'permission_manager_delete'],
+            ],
+
+            'Users Manager' => [
+                ['name' => 'Users Manager List', 'slug' => 'users_manager_list'],
+                ['name' => 'Users Manager Create', 'slug' => 'users_manager_create'],
+                ['name' => 'Users Manager View', 'slug' => 'users_manager_view'],
+                ['name' => 'Users Manager Edit', 'slug' => 'users_manager_edit'],
+                ['name' => 'Users Manager Delete', 'slug' => 'users_manager_delete'],
+            ],
+
+            'Categories Manager' => [
+                ['name' => 'Categories Manager List', 'slug' => 'categories_manager_list'],
+                ['name' => 'Categories Manager Create', 'slug' => 'categories_manager_create'],
+                ['name' => 'Categories Manager View', 'slug' => 'categories_manager_view'],
+                ['name' => 'Categories Manager Edit', 'slug' => 'categories_manager_edit'],
+                ['name' => 'Categories Manager Delete', 'slug' => 'categories_manager_delete'],
+            ],
+
+            'Pages Manager' => [
+                ['name' => 'Pages Manager List', 'slug' => 'pages_manager_list'],
+                ['name' => 'Pages Manager Create', 'slug' => 'pages_manager_create'],
+                ['name' => 'Pages Manager View', 'slug' => 'pages_manager_view'],
+                ['name' => 'Pages Manager Edit', 'slug' => 'pages_manager_edit'],
+                ['name' => 'Pages Manager Delete', 'slug' => 'pages_manager_delete'],
+            ],
+
+            'Emails Manager' => [
+                ['name' => 'Emails Manager List', 'slug' => 'emails_manager_list'],
+                ['name' => 'Emails Manager Create', 'slug' => 'emails_manager_create'],
+                ['name' => 'Emails Manager View', 'slug' => 'emails_manager_view'],
+                ['name' => 'Emails Manager Edit', 'slug' => 'emails_manager_edit'],
+                ['name' => 'Emails Manager Delete', 'slug' => 'emails_manager_delete'],
+            ],
+
+            'Faqs Manager' => [
+                ['name' => 'Faqs Manager List', 'slug' => 'faqs_manager_list'],
+                ['name' => 'Faqs Manager Create', 'slug' => 'faqs_manager_create'],
+                ['name' => 'Faqs Manager View', 'slug' => 'faqs_manager_view'],
+                ['name' => 'Faqs Manager Edit', 'slug' => 'faqs_manager_edit'],
+                ['name' => 'Faqs Manager Delete', 'slug' => 'faqs_manager_delete'],
+            ],
+
+            'Banners Manager' => [
+                ['name' => 'Banners Manager List', 'slug' => 'banners_manager_list'],
+                ['name' => 'Banners Manager Create', 'slug' => 'banners_manager_create'],
+                ['name' => 'Banners Manager View', 'slug' => 'banners_manager_view'],
+                ['name' => 'Banners Manager Edit', 'slug' => 'banners_manager_edit'],
+                ['name' => 'Banners Manager Delete', 'slug' => 'banners_manager_delete'],
+            ],
+
+            'Settings Manager' => [
+                ['name' => 'Settings Manager List', 'slug' => 'settings_manager_list'],
+                ['name' => 'Settings Manager Create', 'slug' => 'settings_manager_create'],
+                ['name' => 'Settings Manager View', 'slug' => 'settings_manager_view'],
+                ['name' => 'Settings Manager Edit', 'slug' => 'settings_manager_edit'],
+                ['name' => 'Settings Manager Delete', 'slug' => 'settings_manager_delete'],
+            ],
+
+            'Seo Manager' => [
+                ['name' => 'Seo Manager List', 'slug' => 'seo_manager_list']
+           
+            ],
+        ],
+    ],
+];
